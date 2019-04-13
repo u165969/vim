fa81e00904feab3dc8212387f73078cc8e3b358a
u165969 <u165969@gmail.com>
2019-04-14 01:15:43 +0900

    add vim config files for vim-go

---
+++ .vimrc
@@ -0,0 +1,6 @@
+call plug#begin('~/.vim/plugged')
+"-------------------------------------------------
+" golang
+"-------------------------------------------------
+Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
+call plug#end()
---
+++ after_plugin_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#vim-go
+cat .vim/plugged/vim-go/templates/hello_world.go > ~/.vim/plugged/vim-go/templates/hello_world.go
+cat .vim/plugged/vim-go/templates/hello_world_test.go >  ~/.vim/plugged/vim-go/templates/hello_world_test.go
