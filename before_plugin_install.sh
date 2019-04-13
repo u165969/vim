02a28b3d4f99338affcef5c0eb4e276e4dc8b43f
u165969 <u165969@gmail.com>
2019-04-14 03:44:10 +0900

    add copy .vimrc in shellscript

--- before_plugin_install.sh
+++ before_plugin_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cp .vimrc ~/.vimrc
+
 #install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#mkdir for a colorscheme plugin
+mkdir ~/.vim/colors
